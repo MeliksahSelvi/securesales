433db80919e3a33e2d519e69505f7d7202055dba
mselvi <mselvi@uni-yaz.com>
2023-08-30 16:54:10 +0300

    Initial commit

---
+++ src/test/java/com/meliksah/securesales/SecuresalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meliksah.securesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuresalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
