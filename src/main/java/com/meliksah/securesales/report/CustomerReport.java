558878828f380144872ae8bc0fc9d7b787d1bd53
mselvi <mselvi@uni-yaz.com>
2023-09-15 23:31:42 +0300

    -added excel report

---
+++ src/main/java/com/meliksah/securesales/report/CustomerReport.java
@@ -0,0 +1,89 @@
+package com.meliksah.securesales.report;
+
+import com.meliksah.securesales.domain.Customer;
+import com.meliksah.securesales.enumeration.ErrorMessage;
+import com.meliksah.securesales.exception.ApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.InputStreamResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static com.meliksah.securesales.constants.Constants.DATE_FORMAT;
+import static org.apache.commons.lang3.time.DateFormatUtils.format;
+
+/**
+ * @Author mselvi
+ * @Created 29.08.2023
+ */
+
+@Slf4j
+public class CustomerReport {
+
+    private XSSFWorkbook workbook;
+    private XSSFSheet sheet;
+    private List<Customer> customers;
+    private static String[] HEADERS = {"Id", "Name", "Email", "Type", "Status", "Address", "Phone", "Created At"};
+
+
+    public CustomerReport(List<Customer> customers) {
+        this.customers = customers;
+        workbook = new XSSFWorkbook();
+        sheet = workbook.createSheet("Customers");
+        setHeaders();
+    }
+
+    private void setHeaders() {
+        Row headerRow = sheet.createRow(0);
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight(14);
+        style.setFont(font);
+        IntStream.range(0, HEADERS.length).forEach(index -> {
+            Cell cell = headerRow.createCell(index);
+            cell.setCellValue(HEADERS[index]);
+            cell.setCellStyle(style);
+        });
+    }
+
+    public InputStreamResource export() {
+        return generateReport();
+    }
+
+    private InputStreamResource generateReport() {
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+
+            CellStyle style = workbook.createCellStyle();
+            XSSFFont font = workbook.createFont();
+            font.setFontHeight(10);
+            style.setFont(font);
+            int rowIndex = 1;
+            for (Customer customer : customers) {
+                Row row = sheet.createRow(rowIndex++);
+                row.createCell(0).setCellValue(customer.getId());
+                row.createCell(1).setCellValue(customer.getName());
+                row.createCell(2).setCellValue(customer.getEmail());
+                row.createCell(3).setCellValue(customer.getType());
+                row.createCell(4).setCellValue(customer.getStatus());
+                row.createCell(5).setCellValue(customer.getAddress());
+                row.createCell(6).setCellValue(customer.getPhone());
+                row.createCell(7).setCellValue(format(customer.getCreatedAt(), DATE_FORMAT));
+            }
+
+            workbook.write(out);
+            return new InputStreamResource(new ByteArrayInputStream(out.toByteArray()));
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            throw new ApiException(ErrorMessage.UNABLE_TO_EXPORT_REPORT_FILE);
+        }
+    }
+}
