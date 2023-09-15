26311d756c57537b2aac3e6cf596a621fed1fb5a
mselvi <mselvi@uni-yaz.com>
2023-09-15 12:25:12 +0300

    -refactor

--- src/main/java/com/meliksah/securesales/security/CustomAuthorizationFilter.java
+++ src/main/java/com/meliksah/securesales/security/CustomAuthorizationFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,7 +27,6 @@ import static org.apache.logging.log4j.util.Strings.EMPTY;
  */
 
 @Component
-@Slf4j
 @RequiredArgsConstructor
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
